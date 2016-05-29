5e440037c6d36e3020bfebee382dbfd01facb727
Hendrik Purmann <h.purmann@googlemail.com>
2016-05-29 23:11:51 +0200

    Add initial Actano Javascript eslint config
    Mainly derived from airbnb, small exceptions added.

---
+++ index.js
@@ -0,0 +1,15 @@
+module.exports = {
+    "env": {
+        "browser": true
+    },
+    "extends": [
+      "eslint-config-airbnb"
+    ].map(require.resolve),
+    "rules": {
+        "semi": ["error", "never"],
+        "no-underscore-dangle": "off",
+        "no-constant-condition": "off",
+        "no-use-before-define": "off",
+        "no-iterator": "off"
+    }
+}
