8ede92c228702bf1f29cb30d5ad974a28e9b54b9
Daniel Merz <Daniel.Merz@Actano.de>
2022-04-07 09:10:57 +0200

    Reorder react rules

--- packages/eslint-config-actano/index.js
+++ packages/eslint-config-actano/index.js
@@ -18,16 +18,16 @@ module.exports = {
     "no-restricted-syntax": ["off", "ForOfStatement"],
     "explicit-function-return-type": "off",
     "implicit-arrow-linebreak": "off", // We don't want to enforce line break usage in arrow functions
-    "react/destructuring-assignment": "off", // We are perfectly fine with using props.myproperty in React components
     "no-return-await": "off",
     "import/prefer-default-export": "off",
-    "react/forbid-prop-types": ["error", { "forbid": ["any", "array"] }],
-    "react-hooks/rules-of-hooks": "error",
-    "react-hooks/exhaustive-deps": "error",
     "mocha-no-only/mocha-no-only": "error",
     "arrow-parens": ["error", "as-needed", { "requireForBlockBody": true }],
     // react
     "react/function-component-definition": "off",
     "react/require-default-props": "off",
+    "react/forbid-prop-types": ["error", { "forbid": ["any", "array"] }],
+    "react-hooks/rules-of-hooks": "error",
+    "react-hooks/exhaustive-deps": "error",
+    "react/destructuring-assignment": "off", // We are perfectly fine with using props.myproperty in React components
   }
 }
